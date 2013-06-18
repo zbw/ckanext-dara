1661784587bc943a00c9f8de52463cf1db16ff9d
Hendrik Bunke <h.bunke@zbw.eu>
2013-06-18 14:39:23 +0200

    improved structure for dara metadata
    -   added separate module for dara md
    -   constructed dictionaries for each level
    -   names for dara md

---
+++ ckanext/dara/md_schema.py
@@ -0,0 +1,47 @@
+#HB 2013-06-17
+#
+
+
+## Level 1 just contains fields that are not in standard CKAN fields. Title,
+# Author are not in here.
+
+level_1 = {
+        'PublicationDate' : 
+            {'name' : 'Publication Date'},
+        'Availabilitycontrolled' : 
+            {'name' : 'Availability (controlled)'},
+        }
+
+level_2 = {
+    'AlternativeIdentifier_ID': 
+        {'name': 'Alternative Identifier'},
+    'AlternativeIdentifier_Type': 
+        {'name': 'Type of Alternative Identifier'},
+    'Availabilityfree': 
+        {'name': 'Availability (free)'},
+    'CollectionDate_free': 
+        {'name': 'Temporal Coverage (free)'},
+    'CollectionMode_free': 
+        {'name': 'Collection Mode (free)'},
+    'DataCollector_name': 
+        {'name': 'Data Collector'},
+    'Note_text': 
+        {'name': 'Notes'},
+    'OtherTitle': 
+        {'name': 'Other Titles'},
+    'Rights': 
+        {'name': 'Rights'},
+    'SelectionMethod': 
+        {'name': 'Sampling'},
+    'TimeDimension_free': 
+        {'name': 'Time Dimension (free)'},
+    'Universe_area': 
+        {'name': 'Geographic Coverage'},
+    'Universe_sampled': 
+        {'name': 'Sampled Universe'},
+    'currentVersion': 
+        {'name': 'Version'},
+    'language': 
+        {'name': 'Language'}
+    }
+
--- ckanext/dara/plugin.py
+++ ckanext/dara/plugin.py
@@ -9,30 +9,11 @@ import ckan.plugins.toolkit as tk
 from pylons import c
 #from pylons import h
 #from ckan.lib.navl.dictization_functions import missing, StopOnError, Invalid
-
+from ckanext.dara.md_schema import level_1, level_2
 
 
 ###TODO write list for mandatory fields. choose the original names from dara
 #schema
-
-
-dara_optional = [
-        'OtherTitle', 
-        'currentVersion', 
-        'language', 
-        'AlternativeIdentifier_ID',
-        'AlternativeIdentifier_Type', 
-        'Universe_area', 
-        'Universe_sampled',
-        'SelectionMethod', 
-        'CollectionDate_free', 
-        'TimeDimension_free',
-        'DataCollector_name', 
-        'CollectionMode_free', 
-        'Note_text',
-        'Availabilityfree', 
-        'Rights',]
-
 
 
 def dara_extras():
@@ -57,7 +38,22 @@ def dara_extras():
 
     except:
         return None
-    
+
+
+def dara_md():
+    """
+    returns dara keys with dara names
+    """
+    all_levels = {}
+    for key in level_1:
+        d = 'dara_' + key
+        all_levels[d] = {'name':level_1[key]['name']}
+    for key in level_2:
+        d = 'dara_' + key
+        all_levels[d] = {'name':level_2[key]['name']}
+    return all_levels
+
+
 
 class DaraMetadataPlugin(plugins.SingletonPlugin, tk.DefaultDatasetForm):
     '''
@@ -69,13 +65,17 @@ class DaraMetadataPlugin(plugins.SingletonPlugin, tk.DefaultDatasetForm):
     plugins.implements(plugins.ITemplateHelpers, inherit=True)
     plugins.implements(plugins.IPackageController, inherit=True)
 
-    
+    ###XXX debugging methods XXX
+   
    #def after_update(self,context, pkg_dict):
    #    """
    #    test
    #    """
    #    import pdb; pdb.set_trace()
 
+   #def before_view(self, pkg_dict):
+   #    dmd = dara_md
+   #    import pdb; pdb.set_trace()
 
 
     def update_config(self, config):
@@ -87,7 +87,8 @@ class DaraMetadataPlugin(plugins.SingletonPlugin, tk.DefaultDatasetForm):
 
     def get_helpers(self):
         #return {'country_codes': country_codes}
-        return {'dara_extras' : dara_extras}
+        return {'dara_extras' : dara_extras, 'dara_md' : dara_md,}
+       
         #return {}
 
     def is_fallback(self):
@@ -103,38 +104,22 @@ class DaraMetadataPlugin(plugins.SingletonPlugin, tk.DefaultDatasetForm):
     def _dara_package_schema(self, schema):
         # Add our custom metadata field to the schema.
        
-        schema.update({
-            'dara_year' : [
-                tk.get_validator('not_empty'),
-                tk.get_converter('convert_to_extras')
-                ]
-            })
-
-        schema.update({
-               'dara_resourceType': [
+        #mandatory fields
+        for key in level_1:
+            name = level_1[key]['name']
+            field_name = 'dara_' + key
+            schema.update({
+                field_name : [
                    tk.get_validator('not_empty'),
                    tk.get_converter('convert_to_extras')
-                   ]
-               })
+                  ]
+              })
 
-        schema.update({
-            'dara_DOI': [
-                tk.get_validator('not_empty'),
-                tk.get_converter('convert_to_extras')
-                ]
-            })
-
-        schema.update({
-            'dara_availability' : [
-                tk.get_validator('not_empty'),
-                tk.get_converter('convert_to_extras')
-                ]
-            })
 
         ### optional fields ###
         #XXX very basic here. what about validation???
-        for element in dara_optional:
-            field_name = 'dara_' + element
+        for key in level_2:
+            field_name = 'dara_' + key
             schema.update({
                 field_name : [
                     tk.get_validator('ignore_missing'),
@@ -142,16 +127,10 @@ class DaraMetadataPlugin(plugins.SingletonPlugin, tk.DefaultDatasetForm):
                     ]
                 })
 
-       #schema.update({
-       #    'dara_OtherTitle' : [
-       #        tk.get_validator('ignore_missing'),
-       #        tk.get_converter('convert_to_extras'),
-       #        ]
-       #    })
-
 
         return  schema
-               
+    
+
     def create_package_schema(self):
         schema = super(DaraMetadataPlugin, self).create_package_schema()
         schema = self._dara_package_schema(schema)
@@ -163,57 +142,53 @@ class DaraMetadataPlugin(plugins.SingletonPlugin, tk.DefaultDatasetForm):
         schema = self._dara_package_schema(schema)
         return schema
 
+
     def show_package_schema(self):
         schema = super(DaraMetadataPlugin, self).show_package_schema()
 
         ###mandatory fields ###
 
-        schema.update({
-               'dara_resourceType': [
+        for key in level_1:
+            name = level_1[key]['name']
+            field_name = 'dara_' + key
+            schema.update({
+                field_name : [
                    tk.get_converter('convert_from_extras'),
                    tk.get_validator('not_empty'),
-                   ]
-               })
-
-        schema.update({
-            'dara_year': [
-                tk.get_converter('convert_from_extras'),
-                tk.get_validator('not_empty'),
-                ]
-            })
-
-        schema.update({
-            'dara_DOI': [
-                tk.get_converter('convert_from_extras'),
-                tk.get_validator('not_empty'),
-                ]
-            })
-
-        schema.update({
-            'dara_availability' : [
-                tk.get_converter('convert_from_extras'),
-                tk.get_validator('not_empty'),
-                ]
-            })
+                  ]
+              })
 
 
-        #XXX very basic here. what about validation???
-        for element in dara_optional:
-            field_name = 'dara_' + element
+      #     'dara_year': [
+      #         tk.get_converter('convert_from_extras'),
+      #         tk.get_validator('not_empty'),
+      #         ]
+      #     })
+
+      # schema.update({
+      #     'dara_DOI': [
+      #         tk.get_converter('convert_from_extras'),
+      #         tk.get_validator('not_empty'),
+      #         ]
+      #     })
+
+      # schema.update({
+      #     'dara_availability' : [
+      #         tk.get_converter('convert_from_extras'),
+      #         tk.get_validator('not_empty'),
+      #         ]
+      #     })
+
+        for key in level_2:
+            field_name = 'dara_' + key
             schema.update({
                 field_name : [
                     tk.get_converter('convert_from_extras'),
-                    tk.get_validator('ignore_missing'),    
+                    tk.get_validator('ignore_missing'),
                     ]
                 })
-        
-       #schema.update({
-       #    'dara_OtherTitle' : [
-       #        tk.get_converter('convert_from_extras'),
-       #        tk.get_validator('ignore_missing')
-       #        ]
-       #    })
-        
+
+
        
         return schema
 

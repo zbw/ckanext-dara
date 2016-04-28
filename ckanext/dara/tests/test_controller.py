62414a9437376dcfb1a9116226c7dcb64d035388
Hendrik Bunke <h.bunke@zbw.eu>
2016-04-28 11:11:11 +0200

    add xml and darapi tests

--- ckanext/dara/tests/test_controller.py
+++ ckanext/dara/tests/test_controller.py
@@ -6,7 +6,12 @@ import ckan.plugins as plugins
 from ckan.tests.helpers import FunctionalTestBase
 #from routes import url_for
 import ckan.plugins.toolkit as tk
+import ckan.tests.legacy as tests
 import untangle
+import pylons.config as config
+import webtest
+import ckan
+from ckanext.dara.controller import darapi
 
 
 def _get_package_action_page(app, package_name, action):
@@ -22,20 +27,43 @@ def _get_package_action_page(app, package_name, action):
     )
     return response
 
-def _dara_testpackage():
-    pkg = factories.Dataset(
-        dara_authors=['Bargfrede','Philipp', '', '', ''],
-        dara_PublicationDate='2002',
-        
-        # this is fake; TODO: test with real DOI generation method in helpers
-        dara_DOI_Proposal="10.1234/fake.234Mt" 
-        )
-    return pkg
+def _get_resource_action_page(app, package_name, resource_id, action):
+    # user = factories.User()
+    # env = {'REMOTE_USER': user['name'].encode('ascii')}
+    
+    pkg_url = tk.url_for(controller='package', action="read", id=package_name)
+    action_url = "{}/resource/{}/{}".format(pkg_url, resource_id, action)
+
+    response = app.get(
+        url=action_url,
+        # extra_environ=env,
+    )
+    return response
 
 
 class TestDaraController(FunctionalTestBase):
     """
     """
+
+    def setup(self):
+        """
+        Create users and organizations
+        """
+        #super(TestDaraController).setup()
+        self.sysadmin = factories.Sysadmin()
+        self.test_member_1 = factories.User()
+        self.test_member_2 = factories.User()
+        self.test_editor = factories.User()
+
+        users = [{'name': self.test_member_1['name'], 'capacity': 'member'},
+                 {'name': self.test_member_2['name'], 'capacity': 'member'},
+                 {'name': self.test_editor['name'], 'capacity': 'editor'}]
+
+        self.test_org_1 = factories.Organization(users=users,
+            name="testjournal")
+        self.test_org_2 = factories.Organization()
+        self.test_package = self._dara_testpackage()
+    
     @classmethod
     def setup_class(cls):
         super(TestDaraController, cls).setup_class()
@@ -49,11 +77,20 @@ class TestDaraController(FunctionalTestBase):
         plugins.unload('dara')
         super(TestDaraController, cls).teardown_class()
    
-    def test_xml(self):
+    def _dara_testpackage(self):
+        pkg = factories.Dataset(
+            dara_authors=['Bargfrede', 'Philipp', '', '', ''],
+            dara_PublicationDate='2002',
+            dara_currentVersion="1",
+            owner_org=self.test_org_1['id']
+        )
+        return pkg
+
+    def test_dataset_xml(self):
         """
-        test dara_xml generation and view
+        test dara_xml generation and view for dataset
         """
-        dataset = _dara_testpackage()
+        dataset = self.test_package
         app = self._get_test_app()
         response = _get_package_action_page(app, dataset['name'], 'dara_xml')
         nt.eq_(response.headers['Content-Type'], 'text/xml; charset=utf-8')
@@ -61,8 +98,28 @@ class TestDaraController(FunctionalTestBase):
         obj = untangle.parse(response.body)
         nt.assert_equals(obj.resource.publicationDate.year.cdata,
                          dataset['dara_PublicationDate'])
-        
-        # TODO test other elements, at the dara requrired (DOIproposal, authors
+    
+    def test_resource_xml(self):
+            
+        app = self._get_test_app()
+        pkg = self.test_package
+        res = factories.Resource(package_id=pkg['id'])
+        response = _get_resource_action_page(app, pkg['name'], res['id'], 'dara_xml')
+        nt.eq_(response.headers['Content-Type'], 'text/xml; charset=utf-8')
+    
+    def test_darapi_dataset(self):
+        dataset = self.test_package
+        app = self._get_test_app()
+        response = _get_package_action_page(app, dataset['name'], 'dara_xml')
+        xml = response.unicode_body
+        import ipdb; ipdb.set_trace()
+        auth = (config.get('ckanext.dara.demo.user'),
+                config.get('ckanext.dara.demo.password'))
+        dara = darapi(auth, xml, test=True, register=True)
+        assert dara in [200, 201]
+
+
+
         
 
 

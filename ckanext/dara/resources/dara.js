81a3777c8d11d9c27cfac3b76ee48b3a802dab91
Hendrik Bunke <h.bunke@zbw.eu>
2017-09-27 15:35:07 +0200

    JEL classification in da|ra XML metadata

--- ckanext/dara/resources/dara.js
+++ ckanext/dara/resources/dara.js
@@ -8,7 +8,10 @@
 
 $(master_slave_input()); // XXX refactor!
 
+
 $(".select_jels").select2({
+// TODO: get JELs with request to econ_ws
+// https://select2.org/data-sources/ajax
     placeholder: "Start typing and select JELs",
     allowClear: true,
 });

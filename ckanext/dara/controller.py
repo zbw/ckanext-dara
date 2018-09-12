62aa81a5646b535bf9a5d0fd41a144947cafcc7e
Franz Osorio <f.osorio@zbw.eu>
2018-09-12 11:53:38 +0000

    Downloading files doesn't require a user to be logged in

--- ckanext/dara/controller.py
+++ ckanext/dara/controller.py
@@ -218,7 +218,6 @@ class DaraController(PackageController):
         """
         Force download of text files
         """
-        self._check_access(id)
         context = {'model': model, 'session': model.Session,
                    'user': c.user or c.author, 'auth_user_obj': c.userobj}
 

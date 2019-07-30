af68c1dbac50e8b11d0cc72b355a8b8f303b559a
franz osorio <f.osorio@zbw.eu>
2019-07-30 15:32:23 +0200

    Update doi validation

--- ckanext/dara/validators.py
+++ ckanext/dara/validators.py
@@ -204,11 +204,11 @@ def dara_doi_validator(key, data, errors, context):
 
     # don't run the check if the package already exists
     # always run
-    #try:
-    #    if 'package' in context.keys():
-    #        return value
-    #except KeyError:
-    #    pass
+    try:
+        if 'package' in context.keys():
+            return value
+    except KeyError:
+        pass
 
     type_ = data.get(('dara_Publication_PIDType', ))
     if type_ == 'DOI':

abb8c25e402af403dce0b0fcb45a14fb6c55ec90
franz osorio <f.osorio@zbw.eu>
2020-01-10 09:16:39 +0100

    Fix Typo

--- ckanext/dara/controller.py
+++ ckanext/dara/controller.py
@@ -311,7 +311,7 @@ def darapi(auth, xml, test=False, register=False):
     headers = {'content-type': 'application/xml;charset=UTF-8'}
     req = requests.post(url, auth=auth, headers=headers, data=xml_encoded,
             params=parameters)
-    log.info("Requesting DOI [{}]: url".format(test, url))
+    log.info("Requesting DOI [{}]: {}".format(test, url))
     log.error(req)
 
     return req.status_code

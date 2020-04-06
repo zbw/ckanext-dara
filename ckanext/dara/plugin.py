57df1d7e51db91179dbff0a3634fd1770bd43a30
franz osorio <f.osorio@zbw.eu>
2020-04-06 10:17:46 +0200

    Add a note

--- ckanext/dara/plugin.py
+++ ckanext/dara/plugin.py
@@ -43,6 +43,7 @@ def feedback_package_update(context, data_dict):
     if context.get('defer_commit', False):
         pass
     else:
+        # message is given on submission to review
         if is_author(package) and package['dara_edawax_review'] != 'false':
             helpers.flash_html('Dataset Created. Uploaded files can be seen under \
                             "Data and Resources." <br> Your files were uploaded \
